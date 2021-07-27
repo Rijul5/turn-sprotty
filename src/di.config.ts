d440550358c324aeab297c8f86f1018f58cebade
Rijul5 <rijulsaini.mait@gmail.com>
2021-07-27 15:20:32 -0400

    updated with new dependencies

--- src/di.config.ts
+++ src/di.config.ts
@@ -4,18 +4,19 @@
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  */
+
+
+import 'sprotty/css/sprotty.css'
+
+
 import { Container, ContainerModule } from "inversify";
-import {
-    ConsoleLogger, ExpandButtonHandler, ExpandButtonView, HtmlRoot,
-    HtmlRootView, LogLevel, PreRenderedElement, PreRenderedView,
-    SCompartment, SCompartmentView, SEdge, SGraph,
-    SGraphView, SLabel, SLabelView, TYPES, boundsModule,
-    buttonModule, configureModelElement, defaultModule, expandModule,
-    exportModule, fadeModule, hoverModule, modelSourceModule, moveModule,
-    openModule, overrideViewerOptions, selectModule, undoRedoModule,
-    viewportModule, SButton
-} from 'sprotty/lib';
-import { popupModelFactory } from "./popup";
+import { ConsoleLogger, ExpandButtonHandler, ExpandButtonView, HtmlRoot,
+    HtmlRootView, LogLevel, PreRenderedElement,
+    PreRenderedView, SCompartment, SCompartmentView, SEdge, SGraph,
+    SGraphView, SLabel, SLabelView, TYPES, configureModelElement,
+    overrideViewerOptions, SButton, CustomFeatures, moveFeature, openFeature,
+    expandFeature, selectFeature, loadDefaultModules } from 'sprotty';
+
 import {
     ResourceNodeView, GoalNodeView, SoftgoalNodeView, BeliefNodeView, TaskNodeView, IndicatorNodeView, HeaderCompartmentView, ModuleNodeView, SoftgoalNodeViewTry,
     ContributeEdgeView, CorrelationEdgeView, DecompositionEdgeView, DependencyEdgeView
@@ -27,21 +28,37 @@ const turnDiagramModule = new ContainerModule((bind, unbind, isBound, rebind) =>
     rebind(TYPES.ILogger).to(ConsoleLogger).inSingletonScope()
     rebind(TYPES.LogLevel).toConstantValue(LogLevel.warn)
     rebind(TYPES.IModelFactory).to(TURNModelFactory).inSingletonScope()
-    bind(TYPES.PopupModelFactory).toConstantValue(popupModelFactory)
+
     const context = { bind, unbind, isBound, rebind };
     configureModelElement(context, 'graph', SGraph, SGraphView);
-    configureModelElement(context, 'turnnode:goal', TURNNode, GoalNodeView)
-    configureModelElement(context, 'turnnode:softgoal', TURNNode, SoftgoalNodeView)
-    configureModelElement(context, 'turnnode:softgoaltry', TURNNode, SoftgoalNodeViewTry)
-    configureModelElement(context, 'turnnode:belief', TURNNode, BeliefNodeView)
-    configureModelElement(context, 'turnnode:actor', TURNNode, ModuleNodeView)
-    configureModelElement(context, 'turnnode:task', TURNNode, TaskNodeView)
-    configureModelElement(context, 'turnnode:indicator', TURNNode, IndicatorNodeView)
-    configureModelElement(context, 'turnnode:resource', TURNNode, ResourceNodeView)
-    configureModelElement(context, 'label:heading', SLabel, SLabelView)
-    configureModelElement(context, 'label:text', SLabel, SLabelView)
-    configureModelElement(context, 'turnlabel:text', TURNLabel, SLabelView)
-    configureModelElement(context, 'label:classHeader', SLabel, SLabelView)
+    const nodeFeatures: CustomFeatures = {
+        disable: [moveFeature],
+        enable: [openFeature]
+    };
+
+    const moduleFeatures: CustomFeatures = {
+        disable: [moveFeature],
+        enable: [openFeature, expandFeature]
+    };
+
+    const labelFeatures: CustomFeatures = {
+        enable: [selectFeature, openFeature]
+    };
+
+
+    configureModelElement(context, 'graph', SGraph, SGraphView);
+    configureModelElement(context, 'turnnode:goal', TURNNode, GoalNodeView, nodeFeatures)
+    configureModelElement(context, 'turnnode:softgoal', TURNNode, SoftgoalNodeView, nodeFeatures)
+    configureModelElement(context, 'turnnode:softgoaltry', TURNNode, SoftgoalNodeViewTry, nodeFeatures)
+    configureModelElement(context, 'turnnode:belief', TURNNode, BeliefNodeView, nodeFeatures)
+    configureModelElement(context, 'turnnode:actor', TURNNode, ModuleNodeView, moduleFeatures)
+    configureModelElement(context, 'turnnode:task', TURNNode, TaskNodeView, nodeFeatures)
+    configureModelElement(context, 'turnnode:indicator', TURNNode, IndicatorNodeView, nodeFeatures)
+    configureModelElement(context, 'turnnode:resource', TURNNode, ResourceNodeView, nodeFeatures)
+    configureModelElement(context, 'label:heading', SLabel, SLabelView, labelFeatures)
+    configureModelElement(context, 'label:text', SLabel, SLabelView, labelFeatures)
+    configureModelElement(context, 'turnlabel:text', TURNLabel, SLabelView, labelFeatures)
+    configureModelElement(context, 'label:classHeader', SLabel, SLabelView, labelFeatures)
     configureModelElement(context, 'comp:comp', SCompartment, SCompartmentView)
     configureModelElement(context, 'comp:classHeader', SCompartment, HeaderCompartmentView)
     configureModelElement(context, 'edge:dependency', SEdge, DependencyEdgeView)
@@ -55,9 +72,8 @@ const turnDiagramModule = new ContainerModule((bind, unbind, isBound, rebind) =>
 
 export default function createContainer(widgetId: string): Container {
     const container = new Container()
-    container.load(defaultModule, selectModule, moveModule, boundsModule, undoRedoModule, viewportModule,
-        hoverModule, fadeModule, exportModule, expandModule, openModule, buttonModule, modelSourceModule,
-        turnDiagramModule)
+    loadDefaultModules(container);
+    container.load(turnDiagramModule);
     //        container.bind(TYPES.ModelSource).to(TheiaDiagramServer).inSingletonScope()
     overrideViewerOptions(container, {
         needsClientLayout: true,
